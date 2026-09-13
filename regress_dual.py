206319474613aebea8951b56b9b3792f34f6c318
starrysky <12696923+code_starrysky@user.noreply.gitee.com>
2026-09-13 12:58:38 +0800

    test: 添加class多态测试（多态已隐式实现）
    - class_polymorphism_test.lm测试通过：
      - 多态方法调用：父类引用指向子类对象，动态分派
      - 数组中的多态：遍历数组调用方法，根据实际类型分派
      - 继承的方法：子类对象调用父类方法
    - 多态已隐式实现：方法调用根据对象的__classname__属性动态查找方法表
    - CC模式暂不支持class，相关测试加入skip列表
    
    测试结果：pass=66, fail=0, skip=18

--- regress_dual.py
+++ regress_dual.py
@@ -34,7 +34,7 @@ SKIP_LIST = {
     "json_type_thread_test.lm", "cond_timeout_test.lm", "lock_test.lm",
     "lock_test2.lm", "threadlocal_test.lm", "gc_return_race.lm",
     "gc_promotion_test.lm", "gc_efficiency_diag.lm", "mem_leak_test.lm",
-    "class_basic_test.lm", "class_inherit_test.lm", "class_super_test.lm",  # CC模式暂不支持class，VM模式已通过
+    "class_basic_test.lm", "class_inherit_test.lm", "class_super_test.lm", "class_polymorphism_test.lm",  # CC模式暂不支持class，VM模式已通过
 }
 
 
